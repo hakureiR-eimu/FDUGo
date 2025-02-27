2bdb3a6ef2b64caeadbd062412e7eb9ab1ddb5d5
CodeOfUsami <zhang0285@foxmail.com>
2025-02-27 17:20:04 +0800

    feat:add 202201

--- 202201/Main.java
+++ 202201/Main.java
@@ -1,29 +1,80 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        long[] speed = new long[n + 1];
-        long[] efficiency = new long[n + 1];
-        for (int i = 1; i <= n; ++i) {
-            speed[i] = sc.nextLong()
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] speedStr1 = br.readLine().split(",");
+        String[] speedStr2 = br.readLine().split(",");
+        int k = Integer.parseInt(br.readLine());
+        int[] speed = new int[n];
+        int[] efficiency = new int[n];
+        for (int i = 0; i < n; ++i) {
+            speed[i] = Integer.parseInt(speedStr1[i]);
+            efficiency[i] = Integer.parseInt(speedStr2[i]);
         }
-        for (int i = 1; i <= n; ++i) {
-            efficiency[i] = sc.nextLong();
+//        System.out.println(n);
+//        for (int i = 0; i < n; ++i) {
+//            System.out.printf("%d ", speed[i]);
+//
+//        }
+//        System.out.print("\n");
+//        for (int i = 0; i < n; ++i) {
+//            System.out.printf("%d ", efficiency[i]);
+//
+//        }
+//        System.out.print("\n");
+//        System.out.println(k);
+        Solution sol = new Solution();
+        int ans = sol.maxPerformance(n, speed, efficiency, k);
+        System.out.println(ans);
+        //sc.close();
+        br.close();
+    }
+}
+
+class Solution {
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        final int mod = (int) (1e9 + 7);
+        if (k == 0) return 0;
+        Person[] persons = new Person[n];
+        for (int i = 0; i < n; ++i) {
+            persons[i] = new Person(speed[i], efficiency[i]);
         }
-        int k=sc.nextInt();
-
-        //01背包
-        //dp[i][j] 代表1-i号中选择i号且挑选j个所能的最大值
-        //初始条件：dp[0][i]代表无人可选,dp[i][0]代表名额不够
-        long[][] dp=new long[n+1][k+1];
-        for(int i=1;i<=n;++i){
-            for(int j=1;j<=k;++j){
-
+        //效率从大往小
+        Arrays.sort(persons, (a, b) -> b.efficiency - a.efficiency);
+        //定一个动一个，枚举效率的最小值，然后求k-1个人速度的最大值，使用小根堆保存最大的k-1个人
+        //使用pq维护最大的k个人的速度
+        //pq在进入循环时候最多k-1个
+        PriorityQueue<Person> pq = new PriorityQueue<>((a, b) -> a.speed - b.speed);
+        long ans = -0x3f3f3f3f;
+        long cur = 0;
+        for (int i = 0; i < n; ++i) {
+            Person person = persons[i];
+            long cur_t = cur + person.speed;
+            long eff = person.efficiency;
+            ans = Math.max(ans, cur_t * eff);
+            cur += person.speed;
+            pq.offer(person);
+            if (pq.size() == k) {
+                Person removePerson = pq.remove();
+                cur -= removePerson.speed;
             }
+
         }
-        sc.close();
+        return (int) (ans % mod);
+    }
+
+    class Person {
+        int speed;
+        int efficiency;
+
+        Person(int speed, int efficiency) {
+            this.speed = speed;
+            this.efficiency = efficiency;
+        }
     }
 }
